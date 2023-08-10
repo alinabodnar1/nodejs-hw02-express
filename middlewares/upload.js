1b26ab80214f01bf96ef6ed4b257ee272a9ab193
alinabodnar1 <malina.bodnar@gmail.com>
2023-08-10 18:43:43 +0300

    fixed Jimp

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,14 +1,32 @@
 const multer = require("multer");
 const path = require("path");
-const Jimp = require('jimp');
+const Jimp = require("jimp");
+const fs = require("fs");
 
-const tmpDir = path.join(__dirname, '../',  'tmp');  // Users/alinka/Desktop/NODE/HW/nodejs-rest-api/tmp
+const tmpDir = path.join(__dirname, "../", "tmp"); // Users/alinka/Desktop/NODE/HW/nodejs-rest-api/tmp
 
-Jimp.read(`${tmpDir}/avatar.jpg`, (err, avatar) => {
-  if (err) throw err;
-  avatar
-    .resize(250, 250) // resize
-    .write(`${tmpDir}/avatar.jpg`); // save
+fs.readdir(tmpDir, (err, files) => {
+  if (err) {
+    console.log("Помилка при читанні папки:", err);
+    return;
+  }
+  files.forEach((file) => {
+    if (
+      file.endsWith(".jpg") ||
+      file.endsWith(".png") ||
+      file.endsWith(".jpeg")
+    ) {
+      const filePath = `${tmpDir}/${file}`;
+      Jimp.read(filePath, (err, image) => {
+        if (err) {
+          console.log(`Помилка при обробці файлу ${file}`);
+          return;
+        }
+        image.resize(250, 250).write(`${tmpDir}/${file}`);
+        console.log(`Файл ${file} був успішно оброблений і збережений`);
+      });
+    }
+  });
 });
 
 // налаштовуємо об'єкт налаштувань для multer
@@ -16,7 +34,7 @@ const multerConfig = multer.diskStorage({
   destination: tmpDir,
   filename: (req, file, cb) => {
     cb(null, file.originalname);
-  }
+  },
 });
 
 // мідлвара
