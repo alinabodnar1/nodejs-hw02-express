c7e957164ba6f4aeae98e9efffdb99639d2fedc0
alinabodnar1 <malina.bodnar@gmail.com>
2023-08-12 10:40:54 +0300

    fixed resize avatar

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,33 +1,7 @@
 const multer = require("multer");
 const path = require("path");
-const Jimp = require("jimp");
-const fs = require("fs");
 
 const tmpDir = path.join(__dirname, "../", "tmp"); // Users/alinka/Desktop/NODE/HW/nodejs-rest-api/tmp
-
-fs.readdir(tmpDir, (err, files) => {
-  if (err) {
-    console.log("Помилка при читанні папки:", err);
-    return;
-  }
-  files.forEach((file) => {
-    if (
-      file.endsWith(".jpg") ||
-      file.endsWith(".png") ||
-      file.endsWith(".jpeg")
-    ) {
-      const filePath = `${tmpDir}/${file}`;
-      Jimp.read(filePath, (err, image) => {
-        if (err) {
-          console.log(`Помилка при обробці файлу ${file}`);
-          return;
-        }
-        image.resize(250, 250).write(`${tmpDir}/${file}`);
-        console.log(`Файл ${file} був успішно оброблений і збережений`);
-      });
-    }
-  });
-});
 
 // налаштовуємо об'єкт налаштувань для multer
 const multerConfig = multer.diskStorage({
